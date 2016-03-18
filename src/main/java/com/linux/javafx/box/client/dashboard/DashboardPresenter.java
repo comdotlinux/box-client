1d4ce72954d74cfae7dc094d48a8aa590efa0bd2
comdotlinux <guru@linux.com>
2016-03-18 07:52:56 +0530

    Further updates

--- src/main/java/com/linux/javafx/box/client/dashboard/DashboardPresenter.java
+++ src/main/java/com/linux/javafx/box/client/dashboard/DashboardPresenter.java
@@ -7,7 +7,15 @@ package com.linux.javafx.box.client.dashboard;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javax.inject.Inject;
 
 /**
  *
@@ -15,13 +23,63 @@ import javafx.fxml.Initializable;
  */
 public class DashboardPresenter implements Initializable {
 
-    private String dashboardHeader;
+    @Inject
+    private boolean rememberme;
+
+    @Inject
+    private String username;
+
+    @Inject
+    private String password;
+
+    @FXML
+    private TitledPane titlePane;
+
+    @FXML
+    private Label dashboardHeader;
+
+    @FXML
+    private CheckBox rememberMe;
+            
+    @FXML
+    private Button login;
+    
+    @FXML
+    private TextField usernameInput;
+    
+    @FXML
+    private PasswordField passwordInput;
+    
+    @FXML
+    private Label usernameLabel;
+    
+    @FXML
+    private Label passwordLabel;
+    
+    @FXML
+    private Label output;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        this.dashboardHeader = rb.getString("dashboard.header");
+        titlePane.setText(rb.getString("title"));
+        dashboardHeader.setText(rb.getString("header"));
+        rememberMe.setText(rb.getString("rememberme"));
+        login.setText(rb.getString("login"));
+        usernameLabel.setText(rb.getString("username"));
+        passwordLabel.setText(rb.getString("password"));
+
+        rememberMe.setSelected(rememberme);
+        usernameInput.setText(username);
+        passwordInput.setText(password);
+
     }
     
+    public void rememberMeChecked(){
+        output.setText("Remember me is Checked");
+    }
     
-    
+    public void loginButtonClicked(){
+        output.setText("Username : " + usernameInput.getText() + " | Password : " + passwordInput.getText());
+    }
+
 }
