fb37723a9d45f6a9cb70a74fd4740954bbedf14a
Guruprasad Kulkarni <guru@linux.com>
2016-03-22 17:43:13 +0530

    Further updates

--- src/main/java/com/linux/javafx/box/client/base/BasePresenter.java
+++ src/main/java/com/linux/javafx/box/client/base/BasePresenter.java
@@ -8,6 +8,7 @@ package com.linux.javafx.box.client.base;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,6 +19,9 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class BasePresenter implements Initializable{
 
     private static final Logger log = getLogger(BasePresenter.class);
+    
+    @Inject
+    javafx.scene.control.TreeView fileTree;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
